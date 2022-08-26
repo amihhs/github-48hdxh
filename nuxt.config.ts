6250798737b5cba32836538b89a6cb410a0775a2
amihhs <amihhs369@gmail.com>
2022-08-26 14:43:49 +0800

    2 files modified, 2 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  components: {
+    dirs: ['components'],
+    transform: {
+      exclude: [/(\.ts)$/],
+    },
+  },
+  extensions: ['.jsx', '.tsx', '.vue'],
+});
